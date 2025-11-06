dfc7deb035d63d36b0d639a3e5697804db170fac
Yuhang-lan04 <yuhanglan.cn@gmail.com>
2025-11-06 19:47:53 +0800

    Update measure.py

--- measure.py
+++ measure.py
@@ -1,5 +1,4 @@
-import numpy as np  # 导入numpy库，用于数值计算
-
+import numpy as np  
 def do_metric(y_prob, label):
     y_prob = y_prob.cpu().detach().numpy()
     average_precision_score = compute_average_precision_score(y_prob, label)
@@ -8,4 +7,5 @@ def do_metric(y_prob, label):
 def compute_average_precision_score(y_prob, label):
 
     ap_sum = np.sum(y_prob == label) / y_prob.size
+
     return ap_sum
